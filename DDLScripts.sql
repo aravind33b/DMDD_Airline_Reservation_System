8c4d90e8ad40864f064617d6fc5629ccf2613aa3
vikash-singh-prac <singh.vikas@northeastern.edu>
2023-03-21 20:58:49 -0400

    Create Table Flight Type

--- DDLScripts.sql
+++ DDLScripts.sql
@@ -1,6 +1,7 @@
 
 -- Table Cusotmer
 --------------------------------------------------------------------
+
 CREATE TABLE CUSTOMER (
   CustomerID NUMBER NOT NULL,
   FirstName VARCHAR2(45) NOT NULL,
@@ -8,4 +9,14 @@ CREATE TABLE CUSTOMER (
   Email VARCHAR2(45) NOT NULL,
   MobileNo VARCHAR2(45) NOT NULL,
   PRIMARY KEY (CustomerID));
-  
+
+
+
+-- Table FLIGHT_TYPE
+--------------------------------------------------------------------
+
+CREATE TABLE FLIGHT_TYPE (
+  FlightTypeID NUMBER NOT NULL,
+  FlightName VARCHAR2(45) NOT NULL,
+  TotalNoOfSeats NUMBER NULL,
+  PRIMARY KEY (FlightTypeID));
