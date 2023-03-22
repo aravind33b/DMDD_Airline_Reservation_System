d710ceac9ca52a0fa74fb894fad4f41531b6edb4
vikash-singh-prac <singh.vikas@northeastern.edu>
2023-03-21 21:29:17 -0400

    Create Table Airports

--- DDLScripts.sql
+++ DDLScripts.sql
@@ -20,3 +20,15 @@ CREATE TABLE FLIGHT_TYPE (
   FlightName VARCHAR2(45) NOT NULL,
   TotalNoOfSeats NUMBER NULL,
   PRIMARY KEY (FlightTypeID));
+
+  
+
+-- Table Airports
+--------------------------------------------------------------------
+CREATE TABLE AIRPORTS (
+  Airports_ID NUMBER NOT NULL,
+  State VARCHAR2(45) NULL,
+  City VARCHAR2(45) NULL,
+  Airport_Code VARCHAR2(45) NOT NULL UNIQUE,
+  Airport_LongName VARCHAR2(100) NOT NULL UNIQUE,
+  PRIMARY KEY (Airports_ID));
