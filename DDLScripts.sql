d86d23ff238ffb6e03fda33176484f8df420cfa5
vikash-singh-prac <singh.vikas@northeastern.edu>
2023-03-21 20:40:29 -0400

    Create Table Customer

--- DDLScripts.sql
+++ DDLScripts.sql
@@ -1,1 +1,11 @@
 
+-- Table Cusotmer
+--------------------------------------------------------------------
+CREATE TABLE CUSTOMER (
+  CustomerID NUMBER NOT NULL,
+  FirstName VARCHAR2(45) NOT NULL,
+  LastName VARCHAR2(45) NOT NULL,
+  Email VARCHAR2(45) NOT NULL,
+  MobileNo VARCHAR2(45) NOT NULL,
+  PRIMARY KEY (CustomerID));
+  
