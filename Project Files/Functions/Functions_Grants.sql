ad32b0942c56d33b85a5f5177e2f6edc1cb8fd5a
shankar-dh <114494327+shankar-dh@users.noreply.github.com>
2023-04-14 13:05:25 -0400

    added grant access on functions

---
+++ Project Files/Functions/Functions_Grants.sql
@@ -0,0 +1,7 @@
+GRANT EXECUTE ON calculate_passenger_count TO airline_admin;
+GRANT EXECUTE ON FLIGHT_SCHEDULE_EXISTS TO airline_admin, customer;
+GRANT EXECUTE ON PROMOTION_EXISTS TO airline_admin, customer;
+GRANT EXECUTE ON PROMOTION_IS_ACTIVE TO airline_admin, customer;
+GRANT EXECUTE ON SEAT_TYPE_EXISTS TO airline_admin, customer;
+GRANT EXECUTE ON TOTAL_SEATS_AVAILABLE TO airline_admin, customer;
+GRANT EXECUTE ON user_exists TO airline_admin;
